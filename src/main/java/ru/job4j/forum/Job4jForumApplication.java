dccb30c3c81d8bdcc400cf8a0cddaedeb8b95e44
c0dered273 <okaschuk@gmail.com>
2021-08-19 21:34:24 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/forum/Job4jForumApplication.java
@@ -0,0 +1,13 @@
+package ru.job4j.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Job4jForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jForumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/job4j/forum/Job4jForumApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Job4jForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
